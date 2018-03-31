81aa8eb06d746f6d9c97e9474177fe24265e8a6e
Kuliev Eduard <kuliev.ed@gmail.com>
2018-03-31 20:16:25 +0300

    database + new classifier

--- bot.py
+++ bot.py
@@ -1,16 +1,22 @@
 import telebot
-
+from database import DB, DBThread
 from Bayes import predict
+from queue import Queue
 
-token = '407983248:AAGoNA--4lrX7FuflwW47Q7Z1Kdh83CMGBo'
-
+token = '481955063:AAGwRfDppnW9FH2LeTUKb6OS9RquTbd3ijs'
 bot = telebot.TeleBot(token)
-data = []
+work_queue = Queue()
+db = DBThread(work_queue, "messages.db")
+db.start()
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, str(predict([message.text])[0][1]))
+    record = (message.message_id, message.from_user.id, message.chat.id, message.from_user.username,
+              message.date, message.text, predict([message.text])[0][1])
+    work_queue.put(("insert", record,))
 
 
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
